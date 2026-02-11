4defbd11ecfa27b9330d2b6f4c07d5dda68f7222
cirospl <splendoreplay67@gmail.com>
2026-02-11 07:39:42 +0100

    Update MemoryMenager.js

--- MemoryMenager.js
+++ MemoryMenager.js
@@ -1,19 +1,9 @@
 export class MemoryManager {
-    static clean(obj) {
+    static dispose(obj) {
         if (obj.geometry) obj.geometry.dispose();
         if (obj.material) {
-            if (Array.isArray(obj.material)) {
-                obj.material.forEach(m => this.disposeMaterial(m));
-            } else {
-                this.disposeMaterial(obj.material);
-            }
-        }
-    }
-
-    static disposeMaterial(m) {
-        m.dispose();
-        for (const key in m) {
-            if (m[key] && m[key].isTexture) m[key].dispose();
+            if (Array.isArray(obj.material)) obj.material.forEach(m => m.dispose());
+            else obj.material.dispose();
         }
     }
 }
